607d995d40f9f190a212c7b6ec429550d12dc534
hanlin <hanlaung@stanford.edu>
2017-02-13 16:27:20 -0800

    Merge branch 'master' into han-branch
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/rey/emsapp/MainActivity.java
+++ app/src/main/java/com/example/rey/emsapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.rey.emsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    // This method starts another Activity to search for the solution to the symptom
+    public void check(View view) {
+        Spinner spinner = (Spinner) findViewById(R.id.symptomList);
+        Intent intent = new Intent(this, SolutionActivity.class);
+        intent.putExtra("symptom", String.valueOf(spinner.getSelectedItem()));
+        Toast.makeText(this, String.valueOf(spinner.getSelectedItem()),Toast.LENGTH_LONG).show();
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/rey/emsapp/SolutionActivity.java
+++ app/src/main/java/com/example/rey/emsapp/SolutionActivity.java
@@ -1,13 +1,34 @@
 package com.example.rey.emsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class SolutionActivity extends AppCompatActivity {
-
+    HashMap<String, String> checkUp = new HashMap();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solution);
+        initMap();
+        displayText();
+
+    }
+    // This method will eventually be replaced by a database
+    private void initMap(){
+        checkUp.put("Back pain", "Surgery time");
+        checkUp.put("Cough", "Cough drop");
+        checkUp.put("Cold", "Don't drink more cold water!");
+    }
+
+    private void displayText(){
+        Intent intent = getIntent();
+        String solution = checkUp.get(intent.getStringExtra("symptom"));
+
+        TextView res = (TextView) findViewById(R.id.txt_solution);
+        res.setText(solution);
     }
 }
